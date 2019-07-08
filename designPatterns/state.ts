facadabb885d68e182cdd80aba552803c4ab71ce
Marjorie <marjorie.ribeiro@bgmrodotec.com.br>
2019-07-08 14:04:37 -0300

    improve state logic, still in progress

--- designPatterns/state.ts
+++ designPatterns/state.ts
@@ -6,9 +6,17 @@ interface State{
 }
 
 class Order {
+    public cancelledOrderState: State;
+    public paymentPendingState: State;
+    public orderShippedState: State;
+    public orderBeingPreparedState: State;
     public currentState: State;
     constructor() {
-
+        this.cancelledOrderState = new CancelledOrderState(this);
+        this.paymentPendingState = new PaymentPendingState(this);
+        this.orderShippedState = new OrderShippedState(this);
+        this.orderBeingPreparedState = new OrderBeingPreparedState(this);
+        this.setState(this.paymentPendingState);
     }
     public setState(state:State){
         this.currentState = state;
@@ -17,4 +25,77 @@ class Order {
     public getState(): State{
        return this.currentState; 
     }
+}
+
+class PaymentPendingState implements State {
+    public order: Order;
+
+    constructor(order: Order){
+        this.order = order;
+    }
+    public cancelOrder() {
+        console.log('cancelling your unpaid order');
+        this.order.setState(this.order.cancelledOrderState);
+    }
+    public verifyPayment() {
+        console.log('payment verified! Shipping soon :)');
+        this.order.setState(this.order.orderBeingPreparedState);
+    }
+    public shipOrder() {
+        console.log('Cannot ship the order when payment is pending');
+    }
+    
+}
+
+class CancelledOrderState implements State {
+    public order: Order;
+    
+    constructor(order: Order){
+        this.order = order;
+    }
+    public cancelOrder() {
+        throw new Error("Method not implemented.");
+    }
+    public verifyPayment() {
+        throw new Error("Method not implemented.");
+    }
+    public shipOrder() {
+        throw new Error("Method not implemented.");
+    }
+}
+
+class OrderBeingPreparedState implements State {
+    public order: Order;
+    
+    constructor(order: Order){
+        this.order = order;
+    }
+    public cancelOrder() {
+        throw new Error("Method not implemented.");
+    }
+    public verifyPayment() {
+        throw new Error("Method not implemented.");
+    }
+    public shipOrder() {
+        throw new Error("Method not implemented.");
+    }
+    
+}
+
+class OrderShippedState implements State {
+    public order: Order;
+    
+    constructor(order: Order){
+        this.order = order;
+    }
+    public cancelOrder() {
+        throw new Error("Method not implemented.");
+    }
+    public verifyPayment() {
+        throw new Error("Method not implemented.");
+    }
+    public shipOrder() {
+        throw new Error("Method not implemented.");
+    }
+    
 }
