9da31597be1b490ab8e44f5377380857d13a9ce6
Marjorie <marjorie.ribeiro@bgmrodotec.com.br>
2019-07-08 10:48:36 -0300

    feat: add partial logic of state method

---
+++ designPatterns/state.ts
@@ -0,0 +1,20 @@
+interface State{
+    order:Order;
+    cancelOrder();
+    verifyPayment();
+    shipOrder();
+}
+
+class Order {
+    public currentState: State;
+    constructor() {
+
+    }
+    public setState(state:State){
+        this.currentState = state;
+    }
+
+    public getState(): State{
+       return this.currentState; 
+    }
+}
